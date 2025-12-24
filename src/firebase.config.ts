3927ff2f9472233562daf677e61e9396eec33161
Rilwan Idowu Suleiman <easyrilwan@gmail.com>
2025-12-24 19:44:19 +0100

    Display and Update User Details 1

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -5,10 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBg1oQJYvDfA6v_UBuQs7H4gkvJwFZEEjM",
   authDomain: "house-marketplace-sublet-4c125.firebaseapp.com",
   projectId: "house-marketplace-sublet-4c125",
-
   storageBucket: "house-marketplace-sublet-4c125.firebasestorage.app",
-  // storageBucket: "house-markplace-sublet-4c125.appspot.com",
-
   messagingSenderId: "546990098316",
   appId: "1:546990098316:web:902d4892f3d165586ff8a9",
 };
@@ -17,3 +14,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+
+/* For Legacy Code */
+// storageBucket: "house-markplace-sublet-4c125.appspot.com",
