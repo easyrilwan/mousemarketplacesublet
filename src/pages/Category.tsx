e1c103c92aaa7107741f1e03eaa9f21f259336b2
Rilwan Idowu Suleiman <easyrilwan@gmail.com>
2026-01-05 22:02:08 +0100

    next

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 
 export default function Category() {
   const { categoryName } = useParams();
-  console.log(categoryName);
+  console.log(categoryName );
 
   return <div>Category</div>;
 }
