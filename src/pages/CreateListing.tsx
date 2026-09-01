f3f76fa701684582572c666d49aa8855e8171002
easyrilwan <easyrilwan@gmail.com>
2026-09-01 18:45:51 +0100

    create listing form

--- src/pages/CreateListing.tsx
+++ src/pages/CreateListing.tsx
@@ -1,5 +1,5 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, type FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Spinner from "../components/Spinner";
@@ -24,6 +24,22 @@ export default function CreateListing() {
     latitude: 0,
     longitude: 0,
   });
+
+  const {
+    type,
+    name,
+    bedrooms,
+    bathrooms,
+    parking,
+    furnished,
+    address,
+    offer,
+    regularPrice,
+    discountedPrice,
+    images,
+    latitude,
+    longitude,
+  } = formData;
 
   const auth = getAuth();
   const navigate = useNavigate();
@@ -56,5 +72,60 @@ export default function CreateListing() {
     );
   }
 
-  return <div>CreateListing</div>;
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const onMutate = (e: FormEvent<HTMLFormElement>) => {};
+
+  return (
+    <div className="space-y-8 px-2 py-4">
+      <header>
+        <p className="text-2xl font-bold">Create a Listing</p>
+      </header>
+
+      <main className="">
+        <form onSubmit={onSubmit} className="space-y-4">
+          <label htmlFor="sale">Sell / Rent</label>
+
+          <div className="space-x-2 font-bold">
+            <button
+              type="button"
+              className={`w-fit cursor-pointer rounded-lg px-8 py-2 ${
+                type === "sale" ? "bg-green-500" : "bg-white"
+              } `}
+              id="type"
+              value="sale"
+              onClick={onMutate}
+            >
+              Sell
+            </button>
+
+            <button
+              type="button"
+              className={`w-fit cursor-pointer rounded-lg px-8 py-2 ${
+                type === "rent" ? "bg-green-500" : "bg-white"
+              } `}
+              id="type"
+              value="rent"
+              onClick={onMutate}
+            >
+              Rent
+            </button>
+          </div>
+
+          <label htmlFor="name ">Name</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={onMutate}
+            maxLength={32}
+            minLength={10}
+            required
+          />
+        </form>
+      </main>
+    </div>
+  );
 }
